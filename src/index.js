26459f3e2b77003c76cf043f588668104add2a4e
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-04-30 22:25:25 +0300

    Add react-router-dom.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import Routes from 'routes';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Welcome to Hooks!</h3>
+      <Router>
+        <Routes />
+      </Router>
+    </div>
+  );
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
