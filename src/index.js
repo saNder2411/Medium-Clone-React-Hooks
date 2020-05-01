08f378500a064973b758609f4c1c8697ced956ad
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-05-01 15:54:56 +0300

    Add top bar.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 import Routes from 'routes';
+import TopBar from 'components/top-bar/top-bar';
 
 function App() {
   return (
     <div className="App">
-      <h3>Welcome to Hooks!</h3>
       <Router>
+        <TopBar />
         <Routes />
       </Router>
     </div>
