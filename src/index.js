d0271ed5421ea391b3520ca49b068fdc5d8d5370
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-05-05 13:04:37 +0300

    Add useRequest and he dependencies.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import ServiceContext from 'service-context/service-context';
-import MediumCloneService from 'services/medium-clone-service/medium-clone-service';
-
-import App from 'components/app/app';
-
-const mediumCloneService = new MediumCloneService();
+import App from "components/app/app";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ServiceContext.Provider value={mediumCloneService}>
-      <Router>
-        <App />
-      </Router>
-    </ServiceContext.Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById(`root`)
 );
--- src/node_modules/service-context/service-context.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ServiceContext = React.createContext();
-
-export default ServiceContext;
