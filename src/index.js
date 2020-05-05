1f73cbbc6f11bea7613198a7326f919a66b8c5c7
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-05-05 19:52:54 +0300

    Add context current user and refactoring containers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { CurrentUserProvider } from "contexts/current-user/current-user";
 
 import App from "components/app/app";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <CurrentUserProvider>
+    <Router>
+      <App />
+    </Router>
+  </CurrentUserProvider>,
   document.getElementById(`root`)
 );
